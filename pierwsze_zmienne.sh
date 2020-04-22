93118c3a8297c883aed48449199f66fff2671f71
agaw <a.wensker@gmail.com>
2020-04-22 22:54:26 +0200

    pierwsze_zmienne.sh

---
+++ pierwsze_zmienne.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# ten program zawiera deklaracje i odwolanie do zmiennych
+
+imie="aga"
+wiek="7"
+
+echo "jak Ci na imie"
+read imie 
+echo aga
+echo "ile masz lat"
+read wiek
+echo 7
+
